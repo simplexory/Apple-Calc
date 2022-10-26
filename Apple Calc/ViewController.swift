7df66012c37c08dfeb18087354d047d7f696bbb9
Hankovich <simplexory@gmail.com>
2022-10-26 19:01:43 +0200

    Initial commit

---
+++ Apple Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Calc
+//
+//  Created by Юра Ганкович on 26.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
