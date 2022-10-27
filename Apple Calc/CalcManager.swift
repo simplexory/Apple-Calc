e45f0767e606bdc047c51437c144ba5ceac4acbd
Hankovich <simplexory@gmail.com>
2022-10-27 17:24:00 +0200

    rename ViewController + added manager class

---
+++ Apple Calc/CalcManager.swift
@@ -0,0 +1,32 @@
+//
+//  CalcManager.swift
+//  Apple Calc
+//
+//  Created by Юра Ганкович on 27.10.22.
+//
+
+import Foundation
+
+class CalcManager {
+    static let shared = CalcManager()
+    init() { }
+    
+    var leftOperand: Float?
+    var rightOperand: Float?
+    
+    func performOperation() -> Float {
+        
+        return Float()
+    }
+    
+    func unaryOperator() -> Float {
+        
+        return Float()
+    }
+    
+    func binaryOperator() -> Float {
+        
+        return Float()
+    }
+    
+}
